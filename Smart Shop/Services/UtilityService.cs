d6f08a095596ad3ae076920870ac9dc6e458e18f
g-larose <larose.g@fromtier.com>
2023-10-05 04:07:21 -0400

    added navbar

--- Smart Shop/Services/UtilityService.cs
+++ Smart Shop/Services/UtilityService.cs
@@ -11,27 +11,31 @@ namespace Smart_Shop.Services
     {
         public string GenerateBuildVersion()
         {
-            var rnd = new Random();
-            var strBuilder = new StringBuilder();
-            for (int i = 0; i < 24; i+= 4)
-            {
-                for (int x = 0; x < 4; x++)
-                {
-                    var randomNumber = rnd.Next(0, 9);
-                    strBuilder.Append(randomNumber);
-                    if (x == 3)
-                        strBuilder.Append('.');
+            //var rnd = new Random();
+            //var strBuilder = new StringBuilder();
+            //for (int i = 0; i < 24; i+= 4)
+            //{
+            //    for (int x = 0; x < 4; x++)
+            //    {
+            //        var randomNumber = rnd.Next(0, 9);
+            //        strBuilder.Append(randomNumber);
+            //        if (x == 3)
+            //            strBuilder.Append('.');
 
-                }
+            //    }
                 
-            }
+            //}
 
-            var results = Enumerable.Range(1, 24)
-                        .Select(r => rnd.Next(0,9))
-                        .ToList();
-            var index = strBuilder.ToString().LastIndexOf(".");
-            var result = strBuilder.ToString().Substring(0, index);
-            return result;
+
+
+            string version = string.Join(".", Enumerable.Range(0, 6)
+                .Select(__ => new Random().Next(1, 1000)
+                .ToString("D4")));
+
+
+            //var index = strBuilder.ToString().LastIndexOf(".");
+           // var result = strBuilder.ToString().Substring(0, index);
+            return version;
         }
     }
 }
